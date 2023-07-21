133603075106892f3e8cbcb3114d012b07e5efb4
Vitor <vitor.wolf20@gmail.com>
2023-07-21 18:56:08 -0300

    implentado lista de produtos na index html

--- assets/program/index.js
+++ assets/program/index.js
@@ -1,5 +1,6 @@
 let produtos = [] // variavel para armazenar os produtos da api
 
+
 const endpointDaAPI = 'https://64adb39eb470006a5ec64d56.mockapi.io/tsteste/produto'
 
 getBuscarProdutosDaApi()
@@ -8,35 +9,54 @@ async function getBuscarProdutosDaApi() {
 
   const res = await fetch(endpointDaAPI); // utiliza o await para esperar a resposta (res) da chamada à API usando fetch
   produtos = await res.json(); // após obter a resposta é convertida em json e armazenado na variavel produtos
-  mostrarProdutos(produtos);
+  mostrarProdutosPorCategoria();
+  produtosSimilares();
 }
 
-const elementoOndeSeráInserido = document.querySelectorAll('.__listarProdutos');
+function mostrarProdutosPorCategoria() {
+  const elementosOndeSerãoInseridos = {
+    'Star Wars': document.getElementById('listar1'),
+    'Consoles': document.getElementById('listar2'),
+    'Diversos': document.getElementById('listar3'),
+  };
 
-const categoriaHtml = document.querySelectorAll('.--categoria');
-const categoriasHtml = [];
-
-categoriaHtml.forEach(elemento => {
-  const categoriaConteudo = elemento.textContent;
-  categoriasHtml.push(categoriaConteudo);
-});
-
-function mostrarProdutos() {
   produtos.forEach(produto => {
-    if (categoriasHtml.includes(produto.categoria)) {
-      elementoOndeSeráInserido.innerHTML += `
-        <li class="__produto">
+    const categoria = produto.categoria;
+    if (elementosOndeSerãoInseridos[categoria]) {
+      elementosOndeSerãoInseridos[categoria].innerHTML += `
+        <li class="__produto" data-id="${produto.id}">
           <img src="${produto.imagemUrl}" alt="${produto.nomeProduto}">
           <p class="--nomeProd">${produto.nomeProduto}</p>
           <p class="--valor">R$${produto.valor},00</p>
-          <p class="--verProdLink"><a href="#">Ver Produto</a></p>
+          <p class="--verProdLink"><a href="produtosSimilares.html?id=${produto.id}">Ver Produto</a></p>
         </li>
       `;
     }
   });
 }
 
+function produtosSimilares(produtosid) {
 
+  const produtoElemento = document.querySelector('.__produto');
+  console.log(produtoElemento)
+  const elementoParaInserirProdutoClicado = document.querySelector('.__prodClicadoContainer')
+  elementoParaInserirProdutoClicado.innerHTML = ''
+
+  elementoParaInserirProdutoClicado.innerHTML += `
+  <figure class="__imgProd">
+                <img src="${produtos.imagemUrl}" alt="">
+            </figure>
+
+            <div class="__infoProd">
+                <p class="__tituloProd">${produtos.nomeProduto}</p>
+                <p class="__valorProd">R$${produtos.valor}</p>
+                <p class="__descriProd">${produtos.descricao}</p>
+            </div>
+  `
+
+
+
+}
 
 
 
